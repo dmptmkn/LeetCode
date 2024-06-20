794fa06997a54fee432fb7b17ff5dbc85f319dbb
dmptmkn <dm.ptmkn@yandex.ru>
2024-06-20 22:40:31 +0300

    add solution to 'Two Sum' problem

---
+++ src/twosum/Solution.java
@@ -0,0 +1,41 @@
+package twosum;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+
+        nums = new int[]{3, 2, 4};
+        target = 6;
+        result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+
+        nums = new int[]{12, 7, 3, 5, 6, 11};
+        target = 9;
+        result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] indexes = new int[2];
+
+        outer:
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target) continue;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    indexes[0] = i;
+                    indexes[1] = j;
+                    break outer;
+                }
+            }
+        }
+
+        return indexes;
+    }
+}
