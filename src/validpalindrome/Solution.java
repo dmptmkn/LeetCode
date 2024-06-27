f6a1992b9296edd4c99685a77da6bc4f1b482366
dmptmkn <dm.ptmkn@yandex.ru>
2024-06-27 22:41:04 +0700

    add optimized solution to 'Valid Palindrome' problem

--- src/validpalindrome/Solution.java
+++ src/validpalindrome/Solution.java
@@ -6,11 +6,33 @@ public class Solution {
         System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(isPalindrome("race a car"));
         System.out.println(isPalindrome("A_ha"));
+        System.out.println(isPalindromeOptimized("A man, a plan, a canal: Panama"));
     }
 
     public static boolean isPalindrome(String s) {
         String processed = s.replaceAll("[\\W_]", "").toLowerCase();
         String processedAndReversed = new StringBuilder(processed).reverse().toString();
         return processed.equals(processedAndReversed);
+    }
+
+    public static boolean isPalindromeOptimized(String s) {
+        char[] chars = s.toCharArray();
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (Character.isLetterOrDigit(chars[i])) {
+                builder.append(Character.toLowerCase(chars[i]));
+            }
+        }
+        String processed = builder.toString();
+
+        chars = processed.toCharArray();
+        builder = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            builder.append(chars[i]);
+        }
+        String reversed = builder.toString();
+
+        return processed.equals(reversed);
     }
 }
