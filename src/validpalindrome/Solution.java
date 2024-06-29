9af550ce7c1e4555383af8ed67a1058261ecde0c
dmptmkn <dm.ptmkn@yandex.ru>
2024-06-29 10:27:54 +0700

    add stack-based solution to 'Valid Palindrome' problem

--- src/validpalindrome/Solution.java
+++ src/validpalindrome/Solution.java
@@ -1,5 +1,7 @@
 package validpalindrome;
 
+import java.util.Stack;
+
 public class Solution {
 
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class Solution {
         System.out.println(isPalindrome("race a car"));
         System.out.println(isPalindrome("A_ha"));
         System.out.println(isPalindromeOptimized("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindromeStack("A man, a plan, a canal: Panama"));
     }
 
     public static boolean isPalindrome(String s) {
@@ -30,6 +33,31 @@ public class Solution {
         builder = new StringBuilder();
         for (int i = chars.length - 1; i >= 0; i--) {
             builder.append(chars[i]);
+        }
+        String reversed = builder.toString();
+
+        return processed.equals(reversed);
+    }
+
+    public static boolean isPalindromeStack(String s) {
+        char[] chars = s.toCharArray();
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (Character.isLetterOrDigit(chars[i])) {
+                builder.append(Character.toLowerCase(chars[i]));
+            }
+        }
+        String processed = builder.toString();
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < processed.length(); i++) {
+            stack.push(processed.charAt(i));
+        }
+
+        builder = new StringBuilder();
+        while (!stack.isEmpty()) {
+            builder.append(stack.pop());
         }
         String reversed = builder.toString();
 
