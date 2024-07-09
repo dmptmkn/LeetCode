e5237342dabecfb6af3a73b4a974f65a224bee99
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-09 18:38:57 +0300

    add solution to "Fizz Buzz" problem

---
+++ src/fizzbuzz/Solution.java
@@ -0,0 +1,26 @@
+package fizzbuzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+        System.out.println(fizzBuzz(5));
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) answer.add("FizzBuzz");
+            else if (i % 3 == 0) answer.add("Fizz");
+            else if (i % 5 == 0) answer.add("Buzz");
+            else answer.add(String.valueOf(i));
+        }
+
+        return answer;
+    }
+}
