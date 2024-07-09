db8fb2abcf3c7e1f3e973c606fa9e2f584379a38
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-09 09:31:57 +0300

    add solution to "Reverse Words in a String III" problem

--- src/reversewordsinastringiii/Solution.java
+++ src/reversewordsinastringiii/Solution.java
@@ -1,24 +1,34 @@
 package reversewordsinastringiii;
 
+import java.util.StringJoiner;
+
 public class Solution {
 
     public static void main(String[] args) {
         System.out.println(reverseWords("Let's take LeetCode contest"));
-        System.out.println(reverseWords("Mr Ding"));
+        System.out.println(reverseWordsShorter("Mr Ding"));
     }
 
     public static String reverseWords(String s) {
-        String[] split = s.split(" ");
-        for (int i = 0; i < split.length; i++) {
-            split[i] = new StringBuilder(split[i]).reverse().toString();
+        String[] words = s.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            words[i] = new StringBuilder(words[i]).reverse().toString();
         }
 
-        StringBuilder builder = new StringBuilder();
-        for (String word : split) {
-            builder.append(word);
-            builder.append(" ");
+        StringBuilder reverseWords = new StringBuilder();
+        for (String word : words) {
+            reverseWords.append(word);
+            reverseWords.append(" ");
         }
 
-        return builder.toString().trim();
+        return reverseWords.toString().trim();
+    }
+
+    public static String reverseWordsShorter(String s) {
+        String[] words = s.split(" ");
+        StringJoiner reverseWords = new StringJoiner(" ");
+        for (String word : words) reverseWords.add(new StringBuilder(word).reverse().toString());
+
+        return reverseWords.toString();
     }
 }
