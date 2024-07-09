1379bf7c6c8e64575dcb79cd355c3939dc079194
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-09 12:49:52 +0300

    add solution to "Valid Anagram" problem

---
+++ src/validanagram/Solution.java
@@ -0,0 +1,20 @@
+package validanagram;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}
