de1746aa0f7012b3895d04a2b95d7df2cc3bbabd
dmptmkn <dm.ptmkn@yandex.ru>
2024-09-19 15:59:14 +0300

    add solution to "Longest Common Prefix" problem

---
+++ src/longestcommonprefix/Solution.java
@@ -0,0 +1,20 @@
+package longestcommonprefix;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println(longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        }
+        return prefix;
+    }
+}
