dfadecb4626c5b7a2fc500e0e122407fbfe05c16
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-14 23:31:46 +0300

    add optimized solutions to "Check if the Sentence Is Pangram" problem

--- src/checkifthesentenceispangram/Solution.java
+++ src/checkifthesentenceispangram/Solution.java
@@ -8,12 +8,28 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(checkIfPangramSet("thequickbrownfoxjumpsoverthelazydog"));
         System.out.println(checkIfPangramSet("leetcode"));
+        System.out.println(checkIfPangramStream("thequickbrownfoxjumpsoverthelazydog"));
+        System.out.println(checkIfPangramStream("leetcode"));
     }
 
     public static boolean checkIfPangramSet(String sentence) {
+        if (sentence.length() < 26) return false;
+
         Set<Character> uniqueChars = new HashSet<>();
         for (char c : sentence.toCharArray()) uniqueChars.add(c);
+
         return uniqueChars.size() == 26;
     }
 
+    public static boolean checkIfPangramStream(String sentence) {
+        if (sentence.length() < 26) return false;
+
+        long uniqueCharsCount = sentence.chars()
+                .filter(Character::isAlphabetic)
+                .map(Character::toLowerCase)
+                .distinct()
+                .count();
+
+        return uniqueCharsCount == 26;
+    }
 }
