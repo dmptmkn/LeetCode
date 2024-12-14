13723e19c4e59b926002e6ea991740bfe31fa9e0
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-14 16:19:30 +0300

    Add bruteforce solution

--- src/containsduplicate/Solution.java
+++ src/containsduplicate/Solution.java
@@ -11,6 +11,18 @@ public class Solution {
         System.out.println(containsDuplicateSet(new int[]{1,2,3,1}));
         System.out.println(containsDuplicateSet(new int[]{1,2,3,4}));
         System.out.println(containsDuplicateMap(new int[]{1,1,1,3,3,4,3,2,4,2}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1,2,3,1}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1,2,3,4}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    }
+
+    public static boolean containsDuplicateBruteForce(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
     }
 
     public static boolean containsDuplicateSet(int[] nums) {
