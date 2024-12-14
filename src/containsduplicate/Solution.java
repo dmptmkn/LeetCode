782a2a5e9fa1712cb7f66b8f7f8c28738c0ffccb
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-15 00:40:21 +0300

    add stream-based solution

--- src/containsduplicate/Solution.java
+++ src/containsduplicate/Solution.java
@@ -1,5 +1,6 @@
 package containsduplicate;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -8,12 +9,16 @@ import java.util.Set;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(containsDuplicateSet(new int[]{1,2,3,1}));
-        System.out.println(containsDuplicateSet(new int[]{1,2,3,4}));
-        System.out.println(containsDuplicateMap(new int[]{1,1,1,3,3,4,3,2,4,2}));
-        System.out.println(containsDuplicateBruteForce(new int[]{1,2,3,1}));
-        System.out.println(containsDuplicateBruteForce(new int[]{1,2,3,4}));
-        System.out.println(containsDuplicateBruteForce(new int[]{1,1,1,3,3,4,3,2,4,2}));
+        System.out.println(containsDuplicateSet(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicateSet(new int[]{1, 2, 3, 4}));
+        System.out.println(containsDuplicateMap(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1, 2, 3, 4}));
+        System.out.println(containsDuplicateBruteForce(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+        System.out.println(containsDuplicateStream(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicateStream(new int[]{1, 2, 3, 4}));
+        System.out.println(containsDuplicateStream(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+
     }
 
     public static boolean containsDuplicateBruteForce(int[] nums) {
@@ -36,5 +41,11 @@ public class Solution {
         for (int num : nums) numNTimes.merge(num, 1, Integer::sum);
         for (int nTimes : numNTimes.values()) if (nTimes > 1) return true;
         return false;
+    }
+
+    public static boolean containsDuplicateStream(int[] nums) {
+        return nums.length != Arrays.stream(nums)
+                .distinct()
+                .count();
     }
 }
