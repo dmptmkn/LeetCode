a01d9e35515eb022278b9f8cdf3d1737acd0d059
dmptmkn <dm.ptmkn@yandex.ru>
2024-12-19 23:57:25 +0300

    add solution to the problem

--- src/longestsubstringwithoutrepeatingcharacters/Solution.java
+++ src/longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -14,9 +14,7 @@ public class Solution {
         int res = 0;
 
         for (int i = 0; i < n; i++) {
-
             boolean[] visited = new boolean[256];
-
             for (int j = i; j < n; j++) {
                 if (visited[s.charAt(j)]) {
                     break;
