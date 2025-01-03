97e2e0f682a37df477be4c01e787cb30b2783e45
dmptmkn <dm.ptmkn@yandex.ru>
2025-01-04 00:51:19 +0300

    add solution to the problem

---
+++ src/kidswiththegreatestnumberofcandies/Solution.java
@@ -0,0 +1,27 @@
+package kidswiththegreatestnumberofcandies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }
+
+        return result;
+    }
+}
